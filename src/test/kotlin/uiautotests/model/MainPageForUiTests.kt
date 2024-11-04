4a3257ebe28ee9ac14e44c96c3e66834da8b29d1
Tatsiana S. <ttsudorgina@gmail.com>
2024-11-04 15:51:05 +0100

    Edit ClassAttributeTest

--- src/test/kotlin/uiautotests/model/MainPageForUiTests.kt
+++ src/test/kotlin/uiautotests/model/MainPageForUiTests.kt
@@ -6,13 +6,18 @@ import org.openqa.selenium.By
 
 class MainPageForUiTests {
     private val dynamicIdButton = By.xpath("//a[text()='Dynamic ID']")
-    private val classAttribute = By.xpath("//a[text()='Class Attribute']")
+    private val classAttributeButton = By.xpath("//a[text()='Class Attribute']")
+    private val loadDelayButton = By.xpath("//a[text()='Class Attribute']")
 
     fun goToDynamicIdPage() {
         element(dynamicIdButton).should(visible).click()
     }
 
     fun goToClassAttributePage() {
-        element(classAttribute).should(visible).click()
+        element(classAttributeButton).should(visible).click()
+    }
+
+    fun goToLoadDelayPage() {
+        element(loadDelayButton).should(visible).click()
     }
 }
