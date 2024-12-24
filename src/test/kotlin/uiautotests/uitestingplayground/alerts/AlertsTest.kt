a8ab7f02f2085e27f24ed7d5c049c9aa38eb958f
Tatsiana S. <ttsudorgina@gmail.com>
2024-12-24 20:58:51 +0100

    Add AlertsTest

---
+++ src/test/kotlin/uiautotests/uitestingplayground/alerts/AlertsTest.kt
@@ -0,0 +1,31 @@
+package uiautotests.uitestingplayground.alerts
+
+import com.codeborne.selenide.Selenide
+import org.junit.jupiter.api.*
+import uiautotests.config.BaseTest
+import uiautotests.model.*
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DisplayName("Тест с всплывающими окнами")
+class AlertsTest: BaseTest() {
+
+    private val mainPageForUiTests = MainPageForUiTests()
+    private val alertsPage = AlertsPage()
+
+    @BeforeAll
+    override fun beforeAll() {
+        super.beforeAll()
+    }
+
+    @BeforeEach
+    override fun beforeEach() {
+        super.beforeEach()
+        Selenide.open("http://uitestingplayground.com/")
+    }
+
+    @Test
+    @DisplayName("Кнопка подтверждения нажимается после появления алерта")
+    fun pressingTheOkButtonAfterTheAlertAppears() {
+
+    }
+}
