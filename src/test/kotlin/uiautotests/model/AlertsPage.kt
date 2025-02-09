4028625ad74c60592d1b51926a50d975e80fd814
Tatsiana S. <ttsudorgina@gmail.com>
2025-02-09 19:40:32 +0100

    Add answerByNonDefaultValueAfterPromptAppears(). AlertsTest is finished.

--- src/test/kotlin/uiautotests/model/AlertsPage.kt
+++ src/test/kotlin/uiautotests/model/AlertsPage.kt
@@ -1,10 +1,11 @@
 package uiautotests.model
 
 import com.codeborne.selenide.Condition
-import com.codeborne.selenide.Selenide.element
-import com.codeborne.selenide.Selenide.switchTo
+import com.codeborne.selenide.Selenide.*
+import com.codeborne.selenide.files.DownloadActions.click
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
+import org.apache.logging.log4j.MarkerManager.clear
 import org.openqa.selenium.By
 import uiautotests.utils.DateHelper.Companion.getCurrentDayOfWeekFormatted
 import kotlin.test.assertEquals
@@ -45,7 +46,6 @@ class AlertsPage {
 
     private fun getTextFromPopup(): String {
         val popup = switchTo().alert()
-        //val popupText = popup.text
         return popup.text
     }
 
@@ -70,6 +70,66 @@ class AlertsPage {
             assertEquals("No", getTextFromPopup())
             logger.info("Нажата кнопка отмены, т.к. сегодня НЕ Пятница")
         }
+    }
+
+    fun getValuesFromPrompt(): List<String> {
+        val popupText = getTextFromPopup()
+        // Выбираем значения в "" и ''
+        val regex = Regex("""(["'])(.*?)(\1)""")
+        val promptValues = regex
+            .findAll(popupText)
+            .map { it.groupValues[2] }
+            .toList()
+        logger.info("Список значений во всплывающем окне: $promptValues")
+        return promptValues
+    }
+
+    fun getDefaultPromptValue(): String? {
+        val popupText = getTextFromPopup()
+        // Выбираем значение после "User value:"
+        val regex = Regex("""User value:\s*(\w+)""")
+        val defaultPromptValue = regex.find(popupText)?.groups?.get(1)?.value
+        logger.info("Дефолтное значение: $defaultPromptValue")
+        return defaultPromptValue
+    }
+
+    fun getNonDefaultValue(promptValues: List<String>, defaultPromptValue: String?): String? {
+
+        if (defaultPromptValue != null && promptValues.contains(defaultPromptValue)) {
+            logger.info("Дефолтное значение найдено в списке во всплывающем окне")
+
+            // Перебираем значения в списке и возвращаем оставшееся несовпадающее
+            for (nonDefaultValue in promptValues) {
+                if (nonDefaultValue != defaultPromptValue) {
+                    logger.info("Недефолтное значение: $nonDefaultValue")
+                    return nonDefaultValue
+                }
+            }
+            logger.info("Все значения в списке во всплывающем окне совпадают с дефолтным значением")
+        } else {
+            logger.info("Дефолтное значение не найдено в списке во всплывающем окне")
+        }
+
+        return null
+    }
+
+    fun setNonDefaultValueInPrompt(nonDefaultValue: String?) {
+        val prompt = switchTo().alert()
+        prompt.apply {
+            click()
+            clear()
+            sendKeys(nonDefaultValue)
+        }
+        logger.info("Вставка значения в поле на всплывающем окне")
+    }
+
+    fun checkValue(nonDefaultValue: String?) {
+        val popupText = getTextFromPopup()
+        val regex = Regex("""User value:\s*(\w+)""")
+        val actualPromptValue = regex.find(popupText)?.groups?.get(1)?.value
+
+        assertEquals(nonDefaultValue, actualPromptValue, "Введено неверное значение: $actualPromptValue")
+        logger.info("Введено недефолтное значение: $nonDefaultValue")
     }
 
 }
